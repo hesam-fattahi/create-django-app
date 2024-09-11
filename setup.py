10fb88a39b7fb450d01a921c03b5efcc63afed25
hesam-fattahi <hesam.fattahi.hf@gmail.com>
2024-09-11 18:14:11 +0330

    project description added for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="start-django-app",
@@ -15,4 +19,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
